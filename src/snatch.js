3ec2edd8301e58c4d56928c88a8630d684b97c0b
Zhicheng Chen <chenzhicheng@dayuwuxian.com>
2019-06-10 10:31:57 +0800

    feat: add mulitu upload

--- src/snatch.js
+++ src/snatch.js
@@ -1,5 +1,7 @@
 const Settings = require('sketch/settings');
 const UI = require('sketch/ui');
+const sketch = require('sketch');
+const BrowserWindow = require('sketch-module-web-view');
 
 export function uploadSelect(context) {
   onRun(context, false);
@@ -7,8 +9,107 @@ export function uploadSelect(context) {
 export function setting(e) {
   onRun(context, true);
 }
+export function uploadAll(context) {
+  const url = Settings.settingForKey('url');
+  const doc = context.document;
 
-function exportLayerAsBitmap(document, layer, scale) {
+  if (!url) {
+    UI.getInputFromUser(
+      'Input the server address.',
+      {
+        initialValue: url || 'https://'
+      },
+      (err, value) => {
+        if (err) {
+          return;
+        }
+        return Settings.setSettingForKey('url', value);
+      }
+    );
+    return;
+  }
+
+  const document = require('sketch/dom').getSelectedDocument();
+  const options = {
+    identifier: 'unique.id1',
+    width: 540,
+    height: 300
+  };
+
+  const artBoards = document.selectedPage.layers.filter(
+    layer => layer.name === '切图' || layer.name === 'slices'
+  )[0];
+
+  if (!artBoards) {
+    return doc.showMessage(
+      'Select page doesn\'t include artBoard with name "切图" or slices.'
+    );
+  }
+
+  const browserWindow = new BrowserWindow(options);
+
+  const layers = artBoards.layers.map(layer => layer.name);
+
+  browserWindow.webContents
+    .executeJavaScript('sendUrl("' + url + '")')
+    .then(res => {
+      console.log(res);
+    });
+
+  browserWindow.webContents
+    .executeJavaScript('sendLayer(' + JSON.stringify(layers) + ')')
+    .then(res => {
+      console.log(res);
+    });
+
+  browserWindow.webContents.on('nativeLog', function(s, id) {
+    let layer = document.selectedPage.layers
+      .filter(layer => layer.name === '切图')[0]
+      .layers.filter(layer => layer.name === s)[0];
+    if (!layer) {
+      return sketch.UI.message(s + ' not found!');
+    }
+    const layerName = layer.name;
+    const bitmap = sketch
+      .export(
+        [
+          document.selectedPage.layers
+            .filter(layer => layer.name === '切图')[0]
+            .layers.filter(layer => layer.name === s)[0]
+        ],
+        { formats: 'png', output: false }
+      )[0]
+      .toString('base64');
+
+    fetch(url + '/sketch/' + id, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: { image: bitmap, name: layerName }
+    })
+      .then(response => response.text())
+      .then(text => {
+        browserWindow.webContents
+          .executeJavaScript('uploadSuccess()')
+          .then(res => {
+            console.log(res);
+          });
+      })
+      .catch(e => {
+        browserWindow.webContents
+          .executeJavaScript('uploadFail()')
+          .then(res => {
+            console.log(res);
+          });
+      });
+  });
+
+  browserWindow.loadURL(require('./ui.html'));
+}
+
+function exportSelectAsBitmap(document, layer, scale) {
   var slice,
     result = {},
     rect = layer.absoluteRect(),
@@ -48,23 +149,23 @@ const onRun = function(context, setting) {
         if (err) {
           return;
         }
-        return  Settings.setSettingForKey('url', value);
+        return Settings.setSettingForKey('url', value);
       }
     );
-    return ;
+    return;
   }
 
   const doc = context.document;
   const selection = context.selection;
+
   if (selection.count() == 0) {
     doc.showMessage('Please select the layer to upload.');
   } else {
     for (let i = 0; i < selection.count(); i++) {
       const layer = selection[i];
       const layerName = layer.name();
-      const { bitmap } = exportLayerAsBitmap(doc, layer, 1);
-
-      fetch(url, {
+      const { bitmap } = exportSelectAsBitmap(doc, layer, 1);
+      fetch(url + '/sketch/arroast', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
